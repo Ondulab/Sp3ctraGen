5df6b5c2c316b1507cb5f0063a8f3328fe0c4d0a
Zhonx <patrick.reybaud>
2025-03-26 17:08:23 +0100

    first commit

---
+++ CISYNTH_App/CISYNTH_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CISYNTH_AppApp.swift
+//  CISYNTH_App
+//
+//  Created by Zhonx on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CISYNTH_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CISYNTH_App/Inc/spectral_generator.h
@@ -0,0 +1,34 @@
+#ifndef SPECTROGRAM_CONFIG_H
+#define SPECTROGRAM_CONFIG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Structure for spectrogram settings
+typedef struct SpectrogramSettings
+{
+    int     fftSize;
+    double  overlap;
+    double  minFreq;
+    double  maxFreq;
+    double  duration;
+    int     sampleRate;
+    double  dynamicRangeDB;
+    double  gammaCorrection;
+    int     enableDithering;
+    double  contrastFactor;
+    int     enableHighBoost;
+    double  highBoostAlpha;
+} SpectrogramSettings;
+
+// C function we want to call from Swift
+int spectral_generator(const SpectrogramSettings *cfg,
+                       const char *inputFile,
+                       const char *outputFile);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
